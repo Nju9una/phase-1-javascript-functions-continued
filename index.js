1b86dd2db01ea6beae2e4480b7bd891380c8d72a
Patrick Njuguna <mpatricknjuguna02@gmail.com>
2024-03-25 14:48:08 +0300

    functions continued js solution

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// Function to create fun activities for Saturdays
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Function to handle Monday work activities
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// Function to wrap an adjective in visual flair
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
